56f9648d921e85a282193fec5d53a409fc8cdee7
Frorpheus <francescofava4@gmail.com>
2025-08-04 20:41:06 +0000

    Lesson 4

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,9 @@
-from flask import Flask
+from flask import Flask # Lets you quickly get things going on the web
 
 app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World"
+
+app.run(debug=True)
